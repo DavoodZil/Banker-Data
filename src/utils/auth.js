4e89a716a6918cf14e36520c6dad847e051f5d66
m1dhun <midhun@sellowpay>
2025-07-30 14:51:18 +0530

    chore:update auth url for testing

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -62,9 +62,9 @@ export const clearAuth = () => {
 export const handleAuthParams = (search) => {
   const searchParams = new URLSearchParams(search);
   const token = searchParams.get("token");
-  const baseUrl = searchParams.get("baseUrl");
+  // const baseUrl = searchParams.get("baseUrl");
   // const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxMDE5IiwianRpIjoiZWM3ODQ1YjlkYTRhNmMyOWI4M2M3Y2E0ZTgxODA3MjllMmFhZmY2MTk1NWRmYjE3NDMwZDEwMDFlNDgwYmM2NDA0MGMxMWEyODdiOWQ4YmUiLCJpYXQiOjE3NTM4NDk2MDQuNDY0MzU3LCJuYmYiOjE3NTM4NDk2MDQuNDY0MzYsImV4cCI6MTc1Mzg4NTYwNC40NDMxNTcsInN1YiI6IjEyMDA3OCIsInNjb3BlcyI6W119.gJq2tVMDuUCKOXxptuhpkiqVzYcVfe3NlPs_jG1M75_Fibu5DJxXbmd9-rkVu3qkZyCSq6YYpo8_0wyLDcNsLFO_sTS-uNnb1abdzVFNB2dEBfiIBXhMqyMK1V6aJC4Al4xawQINpUqbXFgiUQTV3uMcAOYutBWk5TlWA_P4akYXO30_7w2S_nprQbWan3TQKUaZ13WIOCgi7ero8laB7NOV_y_8HxRzQDIdxSKwrP5qvkiEE53p9MqNUXzgG0rujAGBXhD9uzd9DVAcK4dwOlN-KhGorLCgJtzf7ryeMeoQSC2f7p6x3JP7nxwjEeEhxNqjRCp02cooHY9z8wbPTxT8v1u0f05elvKk2YPo05ND5IkJePlrGJ9iQsqBgHa0ZQ2f8JFzfJhjlgY21v5e_HQZLnJLTNT7hBHCQrHkdIzjx_Ifks4iDRpqGpa8wX2BKTjt3-AHKh_XzR5RC4H3rb7IuxSrcYhORgy00Parl5szJ0njiTl656cL7n2cDNEo4L-V2IJkz87-0rHnoZKI6rtb1Yanpgcxon9-WQp_3JsvM0xg6zbkfgILtQ-ctSC4axBTUrlCmdRfAh61GQszXO3ZxmTWtrqn6JID8uhHgf1jduF-VXJ0-JHTdAts1-2jMmettcKg_Ohd-Nf_FmkQ0pnrrUgefHV7AV_IOhDoVxQ";
-  // const baseUrl = "https://OCW-13023-DANISH-2.api.ocw.sebipay.com/api/v4";
+  const baseUrl = "https://OCW-13023-DANISH-2.api.ocw.sebipay.com/api/v4";
   if (token) {
     setAuthToken(token);
     if (baseUrl) {
